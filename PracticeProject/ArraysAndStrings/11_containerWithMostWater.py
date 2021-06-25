91b51eefc39ad6099a5b3bde4ac48a2ec6ff3878
Nikhil Kumar Gupta <nikhilmittal15@gmail.com>
2021-06-25 18:17:34 +0530

    .

---
+++ PracticeProject/ArraysAndStrings/11_containerWithMostWater.py
@@ -0,0 +1,21 @@
+'''
+Created on 25-Jun-2021
+
+@author: nikhi
+'''
+def maxArea(height):
+    left = 0
+    right = len(height) - 1
+    maxArea = 0
+    
+    while left < right:
+        maxArea = max(maxArea, min(height[left], height[right]) * (right - left))
+        
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+    return maxArea
+
+array = [2,3,4,5,18,17,6]
+print(maxArea(array))
